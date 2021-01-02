0a6603222b8708f8de196cc51d89c209da699c21
Aaron Salisbury <aaron.salisbury@live.com>
2021-01-02 01:51:14 -0600

    Added IEnumerable extension to iterate over an IEnumerable and run an action against each item.  Fluent-ish alternative to some explicit for-each loops.

---
+++ Win10Core/Base/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Win10Core.Base.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static void IterateAndAct<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (T item in source)
+            {
+                action(item);
+            }
+        }
+    }
+}
