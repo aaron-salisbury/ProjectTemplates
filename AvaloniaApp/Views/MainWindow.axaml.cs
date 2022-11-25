ae71dd6fea5dae708749948afd0522f18aa6bdd5
Aaron Salisbury <aaron.salisbury@live.com>
2022-11-25 17:46:55 -0600

    Animate hamburger and settings buttons.

--- AvaloniaApp/Views/MainWindow.axaml.cs
+++ AvaloniaApp/Views/MainWindow.axaml.cs
@@ -1,6 +1,11 @@
+using Avalonia;
+using Avalonia.Animation;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.LogicalTree;
+using Avalonia.Styling;
+using Material.Icons.Avalonia;
+using System;
 using System.Linq;
 
 namespace AvaloniaApp.Views
@@ -14,6 +19,25 @@ namespace AvaloniaApp.Views
 
         private void OnHamburgerClick(object sender, RoutedEventArgs e)
         {
+            // Animate button icon.
+            MaterialIcon hamburgerIcon = this.FindControl<MaterialIcon>("HamburgerIcon");
+
+            var animation = new Avalonia.Animation.Animation()
+            {
+                Duration = TimeSpan.FromSeconds(0.2),
+                Children =
+                {
+                    new KeyFrame()
+                    {
+                        Setters = { new Setter(WidthProperty, hamburgerIcon.Width * 0.66) },
+                        KeyTime = TimeSpan.FromSeconds(0.0)
+                    }
+                }
+            };
+
+            animation.RunAsync(hamburgerIcon, null);
+
+            // Update styles of menu, menu buttons, and menu button text.
             Border mainMenuBorder = this.FindControl<Border>("MainMenuBorder");
 
             foreach (ILogical control in mainMenuBorder.GetLogicalDescendants())
@@ -52,6 +76,26 @@ namespace AvaloniaApp.Views
                 mainMenuBorder.Classes.Remove("menuClosed");
                 mainMenuBorder.Classes.Add("menuOpen");
             }
+        }
+
+        private void OnSettingsClick(object sender, RoutedEventArgs e)
+        {
+            MaterialIcon settingsIcon = this.FindControl<MaterialIcon>("SettingsIcon");
+
+            var animation = new Avalonia.Animation.Animation()
+            {
+                Duration = TimeSpan.FromSeconds(0.4),
+                Children =
+                {
+                    new KeyFrame()
+                    {
+                        Setters = { new Setter(Avalonia.Media.RotateTransform.AngleProperty, -180d) },
+                        KeyTime = TimeSpan.FromSeconds(0.0)
+                    }
+                }
+            };
+
+            animation.RunAsync(settingsIcon, null);
         }
     }
 }
