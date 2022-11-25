d05f0bb643ab22e7629aaa93206389869be5747c
Aaron Salisbury <aaron.salisbury@live.com>
2022-11-25 12:42:50 -0600

    Made main menu collapsible (and is starts that way when app opens).

--- AvaloniaApp/Views/MainWindow.axaml.cs
+++ AvaloniaApp/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
+using System.Linq;
 
 namespace AvaloniaApp.Views
 {
@@ -7,6 +10,48 @@ namespace AvaloniaApp.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnHamburgerClick(object sender, RoutedEventArgs e)
+        {
+            Border mainMenuBorder = this.FindControl<Border>("MainMenuBorder");
+
+            foreach (ILogical control in mainMenuBorder.GetLogicalDescendants())
+            {
+                if (control is TextBlock)
+                {
+                    TextBlock textControl = (TextBlock)control;
+                    if (textControl.Classes.Contains("menuTxt"))
+                    {
+                        textControl.IsVisible = !textControl.IsVisible;
+                    }
+                }
+                else if (control is Button)
+                {
+                    Button textButton = (Button)control;
+                    if (textButton.Classes.Contains("menuBtnOpen"))
+                    {
+                        textButton.Classes.Remove("menuBtnOpen");
+                        textButton.Classes.Add("menuBtnClosed");
+                    }
+                    else if (textButton.Classes.Contains("menuBtnClosed"))
+                    {
+                        textButton.Classes.Remove("menuBtnClosed");
+                        textButton.Classes.Add("menuBtnOpen");
+                    }
+                }
+            }
+
+            if (mainMenuBorder.Classes.Contains("menuOpen"))
+            {
+                mainMenuBorder.Classes.Remove("menuOpen");
+                mainMenuBorder.Classes.Add("menuClosed");
+            }
+            else if (mainMenuBorder.Classes.Contains("menuClosed"))
+            {
+                mainMenuBorder.Classes.Remove("menuClosed");
+                mainMenuBorder.Classes.Add("menuOpen");
+            }
         }
     }
 }
