ad0e6235f6592d3a1bdb8d526a0db24af1978fc0
aaron.salisbury@live.com <aaron.salisbury@live.com>
2023-10-24 09:55:57 -0500

    Implement copy guid function.

--- AvaloniaApp.Presentation/Views/Samples/UUIDGeneratorView.axaml.cs
+++ AvaloniaApp.Presentation/Views/Samples/UUIDGeneratorView.axaml.cs
@@ -1,5 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using AvaloniaApp.Presentation.Base.Extensions;
+using AvaloniaApp.Presentation.ViewModels;
 
 namespace AvaloniaApp.Presentation.Views
 {
@@ -9,6 +12,22 @@ namespace AvaloniaApp.Presentation.Views
         {
             InitializeComponent();
             this.SetDataContext(App.Current?.Services);
+        }
+
+        private async void CopyBtn_OnClick(object? sender, RoutedEventArgs e)
+        {
+            if (DataContext is UUIDGeneratorViewModel viewModel && viewModel.UUID != null)
+            {
+                var clipboard = TopLevel.GetTopLevel(this)?.Clipboard;
+
+                if (clipboard != null)
+                {
+                    var dataObject = new DataObject();
+                    dataObject.Set(DataFormats.Text, viewModel.UUID);
+
+                    await clipboard.SetDataObjectAsync(dataObject);
+                }
+            }
         }
     }
 }
