83b1620f44077f92ef5413584bc864906ad65835
Aaron Salisbury <aaron.salisbury@live.com>
2024-10-18 15:59:55 -0500

    Remove my browser launching hack, now that Avalonia has add the HyperlinkButton control.

--- src/DotNet/AvaloniaApp/Views/HomeView.axaml.cs
+++ src/DotNet/AvaloniaApp/Views/HomeView.axaml.cs
@@ -1,9 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using AvaloniaApp.Presentation.Desktop.Base;
 using AvaloniaApp.Presentation.Desktop.Base.Extensions;
-using AvaloniaApp.Presentation.Desktop.ViewModels;
 
 namespace AvaloniaApp.Presentation.Desktop.Views;
 
@@ -13,13 +10,5 @@ public partial class HomeView : UserControl
     {
         InitializeComponent();
         this.SetDataContext((Application.Current as App)?.Services);
-    }
-
-    private void LicenseBtn_Click(object? sender, RoutedEventArgs a)
-    {
-        if (DataContext is HomeViewModel viewModel)
-        {
-            Browser.Open(viewModel.LicenseURL);
-        }
     }
 }
--- src/DotNet/AvaloniaApp/Views/SettingsView.axaml.cs
+++ src/DotNet/AvaloniaApp/Views/SettingsView.axaml.cs
@@ -1,9 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using AvaloniaApp.Presentation.Desktop.Base;
 using AvaloniaApp.Presentation.Desktop.Base.Extensions;
-using AvaloniaApp.Presentation.Desktop.ViewModels;
 
 namespace AvaloniaApp.Presentation.Desktop.Views;
 
@@ -13,21 +10,5 @@ public partial class SettingsView : UserControl
     {
         InitializeComponent();
         this.SetDataContext((Application.Current as App)?.Services);
-    }
-
-    private void PrivacyBtn_Click(object? sender, RoutedEventArgs a)
-    {
-        if (DataContext is SettingsViewModel viewModel)
-        {
-            Browser.Open(viewModel.PrivacyURL);
-        }
-    }
-
-    private void IssuesBtn_Click(object? sender, RoutedEventArgs a)
-    {
-        if (DataContext is SettingsViewModel viewModel)
-        {
-            Browser.Open(viewModel.IssuesURL);
-        }
     }
 }
